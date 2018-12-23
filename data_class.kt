ff01c321d54c0609e8cccc376ae89c80f897237b
angelhr28 <43622761+angelhr28@users.noreply.github.com>
2018-12-22 23:43:24 -0500

    Update data_class.kt

--- data_class.kt
+++ data_class.kt
@@ -1,11 +1,19 @@
-data class Auto (var marca:String)
+data class Auto (var marca:String, private	var puertas:Int){
+    var numero_puertas=puertas
+    get(){
+       return if(field>0) field else 1
+    }
+}
 data class Persona2(var nombre:String, var edad:Int)
 fun main(args: Array<String>){
-    val auto = Auto( marca ="escarabajo")
+    val auto = Auto( marca ="escarabajo",puertas=0)
     var marca:String = auto.marca
 //     auto.marca="fort"
     auto.hashCode()
+    auto.numero_puertas=4
     println(auto.toString())
+    println(auto.numero_puertas)
+   
     val mabel= Persona2("mabel",13)
     val diper=mabel.copy("diper")
     val stand=mabel.copy("stand",34)
